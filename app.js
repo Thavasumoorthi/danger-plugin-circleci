d47aefa9dd04db99af6a79f18906993d0f161d13
Harshini B <harshini.b@tringapps.com>
2025-01-09 12:44:22 +0530

    added confug file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-   console.log("I am thavasumoorthi")
+   console.log("I am thavasumoorthi i am from thanjavur")
   res.send('Hello World! ,I am from backend departments');
 });
 
